e0e1aa157bca52376fd0e9c158cd38dccdad77e1
Shawn P. Serbin <sserbin@bnl.gov>
2022-11-07 15:54:50 -0500

    Init binder build specs

---
+++ binder/install.R
@@ -0,0 +1,7 @@
+### install regular packages
+install.packages("devtools") # for dev
+install.packages(c("here")) # Base
+install.packages("reticulate") # python support in RMarkdown
+install.packages(c("ggplot2","RColorBrewer")) # for plotting
+install.packages(c("rgdal","raster", "terra", "sp")) # Spat packages
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
