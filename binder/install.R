ea3a4b2bac3bd19dc76a8d1dc18f542c1418a4ce
Shawn P. Serbin <sserbin@bnl.gov>
2022-11-07 16:13:03 -0500

    Updates

--- binder/install.R
+++ binder/install.R
@@ -2,6 +2,7 @@
 install.packages("devtools") # for dev
 install.packages(c("here")) # Base
 install.packages("reticulate") # python support in RMarkdown
-install.packages(c("ggplot2","RColorBrewer")) # for plotting
-install.packages(c("rgdal","raster", "terra", "sp")) # Spat packages
-install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+install.packages("tidyverse") # Get the Tidyverse
+install.packages(c("ggplot2","RColorBrewer","leaflet","shinydashboard")) # for plotting
+install.packages(c("rgdal","raster", "terra", "sp")) # Spatial packages
+install.packages(c("rmarkdown", "caTools", "bitops","httr")) # for knitting
